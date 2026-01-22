133e725e7be47d606a999c65f2df08c939fdd3e9
RajanKrGupta <Rajan.KrGupta1984@gmail.com>
2026-01-22 19:11:35 +0530

    removed duplicate testcase

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,14 +7,6 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-
-test('Duplicate has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
